90840ddadf96067b5a027de41507ab52b3122cc6
LeDevNovice <gregory.saison@proton.me>
2026-02-14 17:16:49 +0100

    feat: add idle and configuring states

--- src/machines/chess-clock.machine.ts
+++ src/machines/chess-clock.machine.ts
@@ -1,6 +1,18 @@
-import { setup } from "xstate";
+import { assign, setup } from "xstate";
 
-import type { ChessClockContext, ChessClockEvent, ChessClockInput, ChessClockTag } from "../types";
+import type { ChessClockContext, ChessClockEvent, ChessClockInput, ChessClockTag, PlayerId, PlayerState, TimeControlConfig } from "../types";
+import { DEFAULT_CONFIG } from "../utils/constants";
+
+function createPlayerState(config: TimeControlConfig): PlayerState {
+  return {
+    timeRemaining: config.initialTime,
+    movesPlayed: 0,
+    turnStartedAt: 0,
+    periodsRemaining: config.mode === 'byoyomi' ? config.byoyomiPeriods : 0,
+    periodTimeRemaining: config.mode === 'byoyomi' ? config.byoyomiPeriodTime : 0,
+    isFlagged: false,
+  };
+}
 
 export const chessClockMachine = setup({
   types: {
@@ -9,14 +21,43 @@ export const chessClockMachine = setup({
     input: {} as ChessClockInput,
     tags: {} as ChessClockTag,
   },
-  guards: {},
-  actions: {},
+  guards: {
+    isConfigValid: ({ context }) => {
+      return context.config.initialTime > 0;
+    },
+  },
+  actions: {
+    applyConfig: assign(({ event }) => {
+      if (event.type !== 'CONFIGURE') return {};
+
+      const config: TimeControlConfig = { ...DEFAULT_CONFIG, ...event.config };
+      return {
+        config,
+        playerA: createPlayerState(config),
+        playerB: createPlayerState(config),
+        winner: null,
+        activePlayer: 'A' as PlayerId,
+      };
+    }),  
+  },
   actors: {},
 }).createMachine({
   id: 'zeitnot',
   context: ({ input }) => ({ /* TO-DO */ }),
   initial: 'idle',
   states: {
-    idle: {},
-  },
+    idle: {
+      on: {
+        CONFIGURE: { target: 'configuring', actions: 'applyConfig' }
+      }
+    },
+    configuring: {
+      tags: ['configuring'],
+      on: {
+        CONFIGURE: { actions: 'applyConfig' },
+        START: { target: 'ready', guard: 'isConfigValid' },
+        RESET: { target: 'idle' },
+      }
+    },
+  }
 });
